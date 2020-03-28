461e2d87e423f70c9d1c55c28e49f3dfa8084ccb
YYJeffrey <501934027@qq.com>
2020-03-28 15:43:49 +0800

    fix: 修复了tabs内容闪现问题

---
+++ components/tabpanel/index.js
@@ -0,0 +1,52 @@
+Component({
+  relations: {
+    "../tabs/index": {
+      type: "parent",
+      linked: function (t) {
+        !this.data.parent && this.setData({
+          parent: t
+        })
+      }
+    },
+    "../../miniprogram_npm/lin-ui/combined-tabs/index": {
+      type: "parent",
+      linked: function (t) {
+        !this.data.parent && this.setData({
+          parent: t
+        })
+      }
+    }
+  },
+  properties: {
+    tab: String,
+    subTab: String,
+    subKey: String,
+    key: String,
+    icon: String,
+    iconSize: {
+      type: String,
+      value: "20"
+    },
+    image: Object,
+    picPlacement: {
+      type: String,
+      value: "top"
+    }
+  },
+  observers: {
+    "**": function (t) {
+      this.updateData(t)
+    }
+  },
+  data: {
+    isCurrent: !1,
+    index: 0,
+    parent: null
+  },
+  methods: {
+    updateData() {
+      let t = this.data.parent;
+      t && t.initTabs()
+    }
+  }
+});

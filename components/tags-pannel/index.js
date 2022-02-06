756253fc8bdc79703498408fe7e756d4d10e46b3
YYJeffrey <501934027@qq.com>
2022-02-06 21:21:04 +0800

    feat: 抽离标签面板组件

---
+++ components/tags-pannel/index.js
@@ -0,0 +1,39 @@
+// components/tags-pannel/index.js
+Component({
+  properties: {
+    labels: Array,
+    // 激活的标签ID
+    activeLabelId: {
+      type: Number,
+      value: -1
+    },
+    // 是否下拉弹窗
+    showPopup: {
+      type: Boolean,
+      value: false
+    }
+  },
+  data: {
+
+  },
+  methods: {
+    /**
+     * 切换弹窗显影状态
+     */
+    togglePopup() {
+      this.setData({
+        showPopup: !this.data.showPopup
+      })
+    },
+    /**
+     * 点击标签事件
+     */
+    onTagTap(event) {
+      const activeLabelId = event.target.dataset.labelId
+      this.setData({
+        activeLabelId: activeLabelId
+      })
+      this.triggerEvent("tagtap", { "activeLabelId": activeLabelId })
+    }
+  }
+})

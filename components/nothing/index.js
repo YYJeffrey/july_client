a8335245fdfddd24443059610a63c5dec539cf77
YYJeffrey <501934027@qq.com>
2022-02-06 21:03:12 +0800

    fix: 完善无数据提示组件

--- components/nothing/index.js
+++ components/nothing/index.js
@@ -2,30 +2,16 @@
 
 Component({
   externalClasses: ['nothing-class'],
-  /**
-   * 组件的属性列表
-   */
   properties: {
-    pic: {
-      type: String,
-      value: ''
-    },
+    pic: String,
     tip: {
       type: String,
-      value: '暂时没有数据'
+      value: "暂时没有数据"
     }
   },
-
-  /**
-   * 组件的初始数据
-   */
   data: {
 
   },
-
-  /**
-   * 组件的方法列表
-   */
   methods: {
 
   }

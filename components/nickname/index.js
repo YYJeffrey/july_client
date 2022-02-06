faf04d43f7f8d5593e7f03eab094e773b8954c98
YYJeffrey <501934027@qq.com>
2022-02-06 20:58:44 +0800

    feat: 抽离昵称组件

---
+++ components/nickname/index.js
@@ -0,0 +1,41 @@
+// components/nickname/index.js
+const app = getApp()
+
+Component({
+  externalClasses: ["nickname-class"],
+  properties: {
+    nickname: String,
+    size: {
+      type: Number,
+      value: 28
+    },
+    userId: {
+      type: Number,
+      value: -1
+    },
+    // 是否可链接
+    isLink: {
+      type: Boolean,
+      value: true
+    }
+  },
+  data: {
+
+  },
+  methods: {
+    /**
+     * 点击昵称跳转名片页或授权页
+     */
+    onNicknameTap() {
+      if (app.globalData.userDetail) {
+        wx.navigateTo({
+          url: "/pages/visiting-card/index?userId=" + this.data.userId
+        })
+      } else {
+        wx.navigateTo({
+          url: "/pages/auth/index"
+        })
+      }
+    }
+  }
+})

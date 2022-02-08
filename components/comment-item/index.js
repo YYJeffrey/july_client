01edada15c58fdde8fe843b5528422c357ecaf39
YYJeffrey <501934027@qq.com>
2022-02-08 14:35:20 +0800

    feat: 抽离评论单项组件

---
+++ components/comment-item/index.js
@@ -0,0 +1,34 @@
+// components/comment-item/index.js
+Component({
+  properties: {
+    comment: Object,
+    // 是否存在边框
+    hasBorder: {
+      type: Boolean,
+      value: true
+    },
+    // 是否为内容所有者
+    isOwner: {
+      type: Boolean,
+      value: false
+    }
+  },
+  data: {
+
+  },
+  methods: {
+    /**
+     * 点击评论事件
+     */
+    onCommentTap() {
+      this.triggerEvent("commentTap", { commentId: this.data.comment.id, nickname: this.data.comment.user.nick_name })
+    },
+
+    /**
+     * 点击删除事件
+     */
+    onDeleteTap() {
+      this.triggerEvent("deleteTap", { commentId: this.data.comment.id })
+    }
+  }
+})

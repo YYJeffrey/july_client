8eed0c33ae1f8f7b2e4178a775719983b7ee1e52
YYJeffrey <501934027@qq.com>
2022-02-08 12:57:42 +0800

    feat: 抽离树洞单项组件

---
+++ components/hole-item/index.js
@@ -0,0 +1,20 @@
+// components/hole-item/index.js
+Component({
+  externalClasses: ["hole-item-class"],
+  properties: {
+    hole: Object
+  },
+  data: {
+
+  },
+  methods: {
+    /**
+     * 跳转树洞详情页
+     */
+    gotoHoleDetail() {
+      wx.navigateTo({
+        url: "/pages/hole-detail/index?holeId=" + this.data.hole.id
+      })
+    }
+  }
+})

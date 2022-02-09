b71cb475092c645d282e036167e656037b5d7f3f
YYJeffrey <501934027@qq.com>
2022-02-09 12:57:35 +0800

    refactor: 重构关注Ta的页

--- pages/follower/index.js
+++ pages/follower/index.js
@@ -1,115 +1,89 @@
 // pages/follower/index.js
+import { Paging } from "../../utils/paging"
 const app = getApp()
 const api = app.api
 const wxutil = app.wxutil
-const pageSize = 16 // 每页显示条数
 
 Page({
   data: {
     followerList: [],
-    page: 1,
-    followUserId: -1,
-    isEnd: false, // 是否到底
-    loading: false
+    followerPaging: null, // 关注Ta的分页器
+    title: null // 导航栏标题
   },
 
   onLoad(options) {
-    const followUserId = options.userId
-    const genderText = options.genderText
-    if (genderText) {
-      // 设置标题
-      wx.setNavigationBarTitle({
-        title: "关注" + genderText + "的"
-      })
-    }
-    this.setData({
-      followUserId: followUserId
+    const userId = options.userId
+    const title = options.title
+
+    wx.setNavigationBarTitle({
+      title: title
     })
-    this.getFollowerList(followUserId)
+    this.setData({
+      title: title
+    })
+    this.initFollowerList(userId)
   },
 
   /**
-   * 获取我的关注列表
+   * 初始化关注Ta的列表
    */
-  getFollowerList(followUserId, page = 1, size = pageSize) {
-    const url = api.followingAPI + "follow_user/" + followUserId + "/"
-    let data = {
-      size: size,
-      page: page
-    }
+  async initFollowerList(followUserId) {
+    const followerPaging = new Paging(api.followingAPI + "follow_user/" + followUserId + "/")
+    this.setData({
+      followerPaging: followerPaging,
+    })
+    this.getMoreFollower(followerPaging)
+  },
 
-    if (this.data.isEnd && page != 1) {
+  /**
+   * 获取更多关注Ta的
+   */
+  async getMoreFollower(followerPaging) {
+    const data = await followerPaging.getMore()
+    if (!data) {
       return
     }
-
-    wxutil.request.get(url, data).then((res) => {
-      if (res.code == 200) {
-        const followerList = res.data
-        this.setData({
-          page: (followerList.length == 0 && page != 1) ? page - 1 : page,
-          loading: false,
-          isEnd: (followerList.length == 0 && page != 1) ? true : false,
-          followerList: page == 1 ? followerList : this.data.followerList.concat(followerList)
-        })
-      }
-    })
-  },
-
-  /**
-   * 触底加载
-   */
-  onReachBottom() {
-    const page = this.data.page
-    const followUserId = this.data.followUserId
-
     this.setData({
-      loading: true
+      followerList: data.accumulator
     })
-    this.getFollowerList(followUserId, page + 1)
   },
 
   /**
    * 点击标签
    */
   onTagTap(event) {
-    const index = event.currentTarget.dataset.followIndex
+    const index = event.currentTarget.dataset.index
     const item = this.data.followerList[index]
-    const followUser = item.user
+    const user = item.user
     const hasFollow = item.has_follow
-    const that = this
 
     if (hasFollow) {
       wx.lin.showActionSheet({
-        title: "确定要取消关注" + followUser.nick_name + "吗？",
+        title: "确定要取消关注" + user.nick_name + "吗？",
         showCancel: true,
         cancelText: "放弃",
         itemList: [{
           name: "取消关注",
-          color: "#666",
+          color: "#666"
         }],
-        success() {
-          that.followOrCancel(followUser.id, "取消关注", index)
+        success: () => {
+          this.followOrCancel(user.id, "取消关注", index)
         }
       })
     } else {
-      this.followOrCancel(followUser.id, "关注", index)
+      this.followOrCancel(user.id, "关注", index)
     }
   },
 
   /**
    * 关注或取关
    */
-  followOrCancel(followUserId, msg, index) {
-    const url = api.followingAPI
-    const data = {
-      "follow_user_id": followUserId
-    }
-
-    wxutil.request.post(url, data).then((res) => {
-      if (res.code == 200) {
+  followOrCancel(userId, msg, index) {
+    wxutil.request.post(api.followingAPI, { "follow_user_id": userId }).then((res) => {
+      if (res.code === 200) {
         wx.lin.showMessage({
           type: "success",
-          content: msg + "成功！",
+          content: msg + "成功！"
         })
 
         let followerList = this.data.followerList
@@ -118,40 +92,26 @@ Page({
         this.setData({
           followerList: followerList
         })
-      } else if (res.message == "Can Not Following Yourself") {
-        wx.lin.showMessage({
-          type: "error",
-          content: "不能关注自己",
-        })
       } else {
         wx.lin.showMessage({
           type: "error",
-          content: msg + "失败！",
+          content: msg + "失败！"
         })
       }
     })
   },
 
   /**
-   * 跳转到用户名片页
+   * 触底加载
    */
-  gotoVisitingCard(event) {
-    if (app.globalData.userDetail) {
-      const userId = event.target.dataset.userId
-      wx.navigateTo({
-        url: "/pages/visiting-card/index?userId=" + userId
-      })
-    } else {
-      wx.navigateTo({
-        url: "/pages/auth/index"
-      })
-    }
+  async onReachBottom() {
+    await this.getMoreFollower(this.data.followerPaging)
   },
 
   onShareAppMessage() {
     return {
-      title: "关注我的",
-      path: "/pages/follower/index"
+      title: "主页",
+      path: "/pages/topic/index"
     }
   }
 })

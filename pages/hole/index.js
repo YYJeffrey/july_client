c9a0f11ca10d169163eb4a504495b0a8ecd0e196
YYJeffrey <501934027@qq.com>
2022-02-09 11:33:08 +0800

    refactor: 重构树洞页

--- pages/hole/index.js
+++ pages/hole/index.js
@@ -1,78 +1,56 @@
 // pages/hole/index.js
+import { Paging } from "../../utils/paging"
 const app = getApp()
 const api = app.api
-const wxutil = app.wxutil
-const pageSize = 16 // 每页显示条数
 
 Page({
   data: {
     holes: [],
-    page: 1,
-    loading: false,
-    isEnd: false // 是否到底
+    holePaging: null  // 树洞分页器
   },
 
   onLoad() {
-    this.getHoles()
+    this.initHoles()
   },
 
   /**
-   * 获取树洞
+   * 初始化树洞
    */
-  getHoles(page = 1, size = pageSize) {
-    const url = api.holeAPI
-    let data = {
-      app_id: app.globalData.appId,
-      size: size,
-      page: page
-    }
+  async initHoles() {
+    const holePaging = new Paging(api.holeAPI, { app_id: app.globalData.appId })
+    this.setData({
+      holePaging: holePaging
+    })
+    await this.getMoreHoles(holePaging)
+  },
 
-    if (this.data.isEnd && page != 1) {
+  /**
+   * 获取更多树洞
+   */
+  async getMoreHoles(holePaging) {
+    const data = await holePaging.getMore()
+    if (!data) {
       return
     }
-
-    wxutil.request.get(url, data).then((res) => {
-      if (res.code == 200) {
-        const holes = res.data
-        this.setData({
-          page: (holes.length == 0 && page != 1) ? page - 1 : page,
-          loading: false,
-          isEnd: ((holes.length < pageSize) || (holes.length == 0 && page != 1)) ? true : false,
-          holes: page == 1 ? holes : this.data.holes.concat(holes)
-        })
-      }
+    this.setData({
+      holes: data.accumulator
     })
+  },
+
+  /**
+   * 触底加载
+   */
+  async onReachBottom() {
+    await this.getMoreHoles(this.data.holePaging)
   },
 
   /**
    * 下拉刷新
    */
   onPullDownRefresh() {
-    this.getHoles()
+    this.initHoles()
     wx.stopPullDownRefresh()
     wx.vibrateShort()
-  },
-
-  /**
-   * 触底加载
-   */
-  onReachBottom() {
-    const page = this.data.page
-
-    this.setData({
-      loading: true
-    })
-    this.getHoles(page + 1)
-  },
-
-  /**
-   * 跳转树洞详情页
-   */
-  gotoDetail(event) {
-    const holeId = event.currentTarget.dataset.id
-    wx.navigateTo({
-      url: "/pages/hole-detail/index?holeId=" + holeId
-    })
   },
 
   onShareAppMessage() {

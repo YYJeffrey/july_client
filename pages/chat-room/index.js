b4a469ecea51fb9f575aadeb5a71984659d6168b
YYJeffrey <501934027@qq.com>
2022-02-09 16:18:24 +0800

    refactor: 重构树洞深处聊天室页

--- pages/chat-room/index.js
+++ pages/chat-room/index.js
@@ -1,21 +1,20 @@
 // pages/chat-room/index.js
+import io from "../../utils/socket.io"
 const app = getApp()
 const api = app.api
 const wxutil = app.wxutil
-import io from "../../utils/socket.io"
-
 let socket = null
 
 Page({
   data: {
+    msg: [],
     roomId: null,
     content: null,
     userId: -1,
-    scrollTop: 1800,
-    timeDown: 0, // 大于5分钟消息间隔报时
-    countDown: 0, // 倒计时秒数
-    height: 1116, // 消息内容区高度
-    msg: []
+    height: 1000, // 消息内容区高度
+    toIndex: 0, // 滚动至元素坐标
+    timeDown: 0, // 消息间隔时间
+    countDown: 0 // 聊天室关闭倒计时
   },
 
   onLoad(options) {
@@ -23,13 +22,14 @@ Page({
     const countDown = options.countDown
 
     this.setData({
-      roomId: roomId
+      roomId: roomId,
+      userId: app.globalData.userDetail.id
     })
 
-    this.getCountDown(countDown)
-    this.getUserId()
-    this.getScrollHeight()
     this.getChatList()
+    this.getScrollHeight()
+    this.getCountDown(countDown)
+
     this.connectSocket(roomId)
     this.onSocketMessage("status") // 监听状态信息
     this.onSocketMessage("message") // 监听文字消息
@@ -41,20 +41,42 @@ Page({
   },
 
   /**
-   * 获取窗口高度
+   * 获取聊天列表
    */
-  getScrollHeight() {
-    const that = this;
-    wx.getSystemInfo({
-      success: function (res) {
-        const windowHeight = res.windowHeight;
-        const windowWidth = res.windowWidth;
-        const ratio = 750 / windowWidth;
-        that.setData({
-          height: windowHeight * ratio - 88
+  getChatList() {
+    const msg = this.data.msg
+    let data = {
+      room_id: this.data.roomId
+    }
+    if (msg.length > 0 && "create_time" in msg[0]) {
+      data["create_time"] = msg[0]["create_time"]
+    }
+
+    wxutil.request.get(api.chatResAPI, data).then((res) => {
+      if (res.code === 200) {
+        let data = res.data
+        data.reverse()
+        this, this.setData({
+          msg: data.concat(this.data.msg),
         })
       }
     })
+  },
+
+  /**
+   * 获取窗口高度
+   */
+  getScrollHeight() {
+    const systemInfo = wx.getSystemInfoSync()
+    const windowHeight = systemInfo.windowHeight
+
+    const query = wx.createSelectorQuery()
+    query.select('.edit-item').boundingClientRect(rect => {
+      const editHeight = rect.height
+      this.setData({
+        height: windowHeight - editHeight
+      })
+    }).exec()
   },
 
   /**
@@ -67,53 +89,30 @@ Page({
         countDown: --countDown,
         timeDown: ++timeDown
       })
-      if (countDown == 300) {
+      if (countDown === 300) {
         wx.lin.showMessage({
           content: "树洞将在5分钟后关闭！",
           duration: 3000
         })
       }
-      if (countDown <= 60 && countDown > 3) {
+      else if (countDown <= 60 && countDown > 3) {
         wx.lin.showMessage({
           content: "距离树洞关闭还剩 " + countDown + " 秒！",
           duration: 1000
         })
       }
-      if (countDown == 3) {
+      else if (countDown === 3) {
         wx.lin.showMessage({
           type: "warning",
           content: "树洞即将关闭！",
           duration: 3000
         })
       }
-      if (countDown <= 0) {
+      else if (countDown <= 0) {
         clearInterval(interval)
         wx.navigateBack()
       }
-    }, 1000);
-  },
-
-  /**
-   * 获取用户ID
-   */
-  getUserId() {
-    this.setData({
-      userId: app.globalData.userDetail.id
-    })
-  },
-
-  /**
-   * 连接Socket
-   */
-  connectSocket(roomId) {
-    socket = this.socket = io(api.chatAPI)
-    socket.on("connect", (res) => {
-      const data = {
-        room_id: roomId,
-        token: app.globalData.userDetail.token
-      }
-      this.sendSocketMessage("join", data)
-    })
+    }, 1000)
   },
 
   /**
@@ -124,43 +123,41 @@ Page({
   },
 
   /**
-   * 监听Socket信息
+   * 连接Socket
    */
-  onSocketMessage(path) {
-    this.socket.on(path, (res) => {
-      let msg = this.data.msg
-      let timeDown = this.data.timeDown
-
-      // 间隔时间大于5分钟输出时分秒
-      if (timeDown >= 300) {
-        msg.push({
-          type: "time",
-          content: wxutil.getDateTime().slice(-8, -3)
-        })
+  connectSocket(roomId) {
+    socket = this.socket = io(api.chatAPI)
+    socket.on("connect", () => {
+      const data = {
+        room_id: roomId,
+        token: app.globalData.userDetail.token
       }
-
-      msg.push(res)
-      this.scrollToBottom()
-      this.setData({
-        msg: msg,
-        timeDown: 0
-      })
+      this.sendSocketMessage("join", data)
     })
   },
 
   /**
-   * 滚动至底部
+   * 监听Socket信息
    */
-  scrollToBottom() {
-    const query = wx.createSelectorQuery();
-    query.select(".msg-scroll").boundingClientRect();
-    query.select(".msg-list").boundingClientRect();
-    query.exec((res) => {
-      const scorllHeight = res[0].height;
-      const listHeight = res[1].height;
+  onSocketMessage(path) {
+    this.socket.on(path, (res) => {
+      const msg = this.data.msg
+      const timeDown = this.data.timeDown
+
+      // 消息时间间隔超过5分钟输出当前时间
+      if (timeDown >= 300) {
+        msg.push({
+          type: "time",
+          data: wxutil.getDateTime().slice(-8, -3)
+        })
+      }
+
+      msg.push(res)
       this.setData({
-        scrollTop: listHeight - scorllHeight + 1000
+        msg: msg,
+        timeDown: 0
       })
+      this.scrollToBottom()
     })
   },
 
@@ -178,6 +175,14 @@ Page({
    */
   onSendMessageTap() {
     const content = this.data.content
+    if (!wxutil.isNotNull(content)) {
+      wx.lin.showMessage({
+        type: "error",
+        content: "内容不能为空！"
+      })
+      return
+    }
+
     this.setData({
       content: null
     })
@@ -187,26 +192,21 @@ Page({
   /**
    * 发送图片
    */
-  sendImg(event) {
+  sendImg() {
     wxutil.image.choose(1).then((res) => {
-      if (res.errMsg == "chooseImage:ok") {
-        const url = api.holeAPI + "images/"
-
+      if (res.errMsg === "chooseImage:ok") {
         wxutil.file.upload({
-          url: url,
+          url: api.holeAPI + "images/",
           fileKey: "file",
           filePath: res.tempFilePaths[0]
         }).then((res) => {
-          const data = JSON.parse(res.data);
-          if (data.code == 200) {
+          const data = JSON.parse(res.data)
+          if (data.code === 200) {
             this.sendSocketMessage("images", data.data.url)
-            this.setData({
-              scrollTop: this.data.scrollTop + 1000
-            })
           } else {
             wx.lin.showMessage({
               type: "error",
-              content: "图片上传失败！"
+              content: "发送图片失败！"
             })
           }
         })
@@ -215,42 +215,13 @@ Page({
   },
 
   /**
-   * 获取聊天列表
+   * 自动滚动至底部
    */
-  getChatList() {
-    let msg = this.data.msg
-    let data = {
-      room_id: this.data.roomId
-    }
-    if (msg.length > 0 && "create_time" in msg[0]) {
-      data["create_time"] = msg[0]["create_time"]
-    }
-
-    wxutil.request.get(api.chatResAPI, data).then((res) => {
-      if (res.code == 200) {
-        let data = res.data
-        data.reverse()
-        this, this.setData({
-          msg: data.concat(this.data.msg)
-        })
-      }
-    })
-  },
-
-  /**
-   * 图片预览
-   */
-  previewImage(event) {
-    wx.previewImage({
-      current: "",
-      urls: [event.currentTarget.dataset.src]
-    })
-  },
-
-  /**
-   * 触顶加载
-   */
-  scrolltoupper() {
-    this.getChatList()
-  },
+  scrollToBottom() {
+    setTimeout(() => {
+      this.setData({
+        toIndex: this.data.msg.length - 1
+      })
+    }, 1000)
+  }
 })

a6095a971a1385c198742cb900fa64d870b816f1
YYJeffrey <501934027@qq.com>
2022-05-04 18:26:12 +0800

    fix: 更新了wxutil版本

--- miniprogram_npm/@yyjeffrey/wxutil/index.js
+++ miniprogram_npm/@yyjeffrey/wxutil/index.js
@@ -4,7 +4,7 @@ var __DEFINE__ = function(modId, func, req) { var m = { exports: {}, _tempexport
 var __REQUIRE__ = function(modId, source) { if(!__MODS__[modId]) return require(source); if(!__MODS__[modId].status) { var m = __MODS__[modId].m; m._exports = m._tempexports; var desp = Object.getOwnPropertyDescriptor(m, "exports"); if (desp && desp.configurable) Object.defineProperty(m, "exports", { set: function (val) { if(typeof val === "object" && val !== m._exports) { m._exports.__proto__ = val.__proto__; Object.keys(val).forEach(function (k) { m._exports[k] = val[k]; }); } m._tempexports = val }, get: function () { return m._tempexports; } }); __MODS__[modId].status = 1; __MODS__[modId].func(__MODS__[modId].req, m, m.exports); } return __MODS__[modId].m.exports; };
 var __REQUIRE_WILDCARD__ = function(obj) { if(obj && obj.__esModule) { return obj; } else { var newObj = {}; if(obj != null) { for(var k in obj) { if (Object.prototype.hasOwnProperty.call(obj, k)) newObj[k] = obj[k]; } } newObj.default = obj; return newObj; } };
 var __REQUIRE_DEFAULT__ = function(obj) { return obj && obj.__esModule ? obj.default : obj; };
-__DEFINE__(1651654145677, function(require, module, exports) {
+__DEFINE__(1651659935453, function(require, module, exports) {
 /**
  * @Author: Jeffrey
  * @CreateDate: 2019-04
@@ -406,35 +406,6 @@ const getStorage = key => {
 }
 
 /**
- * getLocation用法：
- * getLocation().then((data) => {})
- * @param {String} type
- * @param {Boolean} watch
- */
-const getLocation = (type = 'gcj02', watch = false) => {
-  return new Promise((resolve, reject) => {
-    wx.getLocation({
-      type: type,
-      success: res => {
-        resolve(res)
-        const latitude = res.latitude
-        const longitude = res.longitude
-        if (watch) {
-          wx.openLocation({
-            latitude,
-            longitude,
-            scale: 18
-          })
-        }
-      },
-      fail: error => {
-        reject(error)
-      }
-    })
-  })
-}
-
-/**
  * getUserProfile用法：
  * getUserProfile().then(res => {})
  * @param {String} lang
@@ -638,7 +609,6 @@ if (!exports.__esModule) Object.defineProperty(exports, "__esModule", { value: t
   showActionSheet,
   setStorage,
   getStorage,
-  getLocation,
   getUserProfile,
   requestPayment,
   autoUpdate,
@@ -649,7 +619,7 @@ if (!exports.__esModule) Object.defineProperty(exports, "__esModule", { value: t
   getUUID
 };
 }, function(modId) {var map = {}; return __REQUIRE__(map[modId], modId); })
-return __REQUIRE__(1651654145677);
+return __REQUIRE__(1651659935453);
 })()
 //miniprogram-npm-outsideDeps=[]
 //# sourceMappingURL=index.js.map

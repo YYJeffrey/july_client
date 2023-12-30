b7caec4565edd38ed633ab527cc2a5bb41128de2
YYJeffrey <501934027@qq.com>
2023-12-30 10:59:05 +0800

    feat: update for v2.

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 //app.js
-import wxutil from "./miniprogram_npm/@yyjeffrey/wxutil/index"
+import wxutil from './miniprogram_npm/@yyjeffrey/wxutil/index'
+import { Auth } from './models/auth'
 
 App({
   globalData: {
     appId: wx.getAccountInfoSync().miniProgram.appId,
-    githubURI: "YYJeffrey/july_client",
-    githubURL: "https://github.com/YYJeffrey/july_client",
-    likeAuthor: "https://img.yejiefeng.com/qr/qr_like.png", // 作者的赞赏码
+    githubURI: 'YYJeffrey/july_client',
+    githubURL: 'https://github.com/YYJeffrey/july_client',
+    likeAuthor: 'https://img.yejiefeng.com/qr/qr_like.png', // 作者的赞赏码
     userDetail: null, // 用户信息详情
-    tokenExpires: 86400 * 28 // Token过期时间
+    tokenExpires: 86400 * 27 // Token过期时间
   },
 
   onLaunch() {
@@ -20,7 +21,17 @@ App({
    * 获取用户详情
    */
   getUserDetail() {
-    const userDetail = wxutil.getStorage("userDetail")
+    const deadtime = wxutil.getStorage('userDetail_deadtime')
+
+    if (deadtime) {
+      const remainTime = parseInt(deadtime) - Date.parse(new Date()) / 1000
+      // 令牌剩余 25% 自动续期
+      if (remainTime <= this.globalData.tokenExpires * 0.25) {
+        this.passiveAuth()
+      }
+    }
+
+    const userDetail = wxutil.getStorage('userDetail')
     if (userDetail) {
       this.globalData.userDetail = userDetail
     } else {
@@ -29,12 +40,28 @@ App({
   },
 
   /**
+   * 被动授权
+   */
+  passiveAuth() {
+    wx.login({
+      success: async (res) => {
+        const info = await Auth.passive(res.code)
+        if (info.code === 0) {
+          const userDetail = info.data
+          wxutil.setStorage('userDetail', userDetail, this.globalData.tokenExpires)
+          this.globalData.userDetail = userDetail
+        }
+      }
+    })
+  },
+
+  /**
    * 获取请求头
    */
   getHeader() {
     let header = {}
     if (this.globalData.userDetail) {
-      header["Authorization"] = "Token " + this.globalData.userDetail.token
+      header['Authorization'] = 'Token ' + this.globalData.userDetail.token
     }
     return header
   },
@@ -43,10 +70,10 @@ App({
    * Token无效跳转授权页
    */
   gotoAuthPage(res) {
-    if (res.message === "Token Is Invalid") {
-      wx.removeStorageSync("userDetail")
+    if (res.code == 10120 || res.code == 10121) {
+      wx.removeStorageSync('userDetail')
       wx.navigateTo({
-        url: "/pages/auth/index",
+        url: '/pages/auth/index'
       })
     }
   }
